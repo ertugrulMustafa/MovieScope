70dbbbccc213dcd26b083ae47ad5a981faa4a900
Mustafa Recep Ertugrul <mustafarecepertugrul@gmail.com>
2023-06-08 13:40:41 +0300

    credentials

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,9 +6,33 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://dy6daoimk5aytntxn52wesvj64.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-qfvudmq3xvcbbcv3lswelzn2lm",
-    "aws_cognito_identity_pool_id":"us-east-1:ee2696cf-ca07-4437-b2e0-48c4b2c1607f",
-    "aws_user_pools_id":"us-east-1_01AbFIfIJ"
+    "aws_appsync_apiKey": "da2-3nnacidngzfsbjiyz6prwcmnpq",
+    "aws_cognito_identity_pool_id": "us-east-1:ee2696cf-ca07-4437-b2e0-48c4b2c1607f",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_01AbFIfIJ",
+    "aws_user_pools_web_client_id": "6nngqppuopja0e5pqev4cqhql5",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": [
+            "REQUIRES_LOWERCASE",
+            "REQUIRES_UPPERCASE",
+            "REQUIRES_NUMBERS",
+            "REQUIRES_SYMBOLS"
+        ]
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
 };
 
 
